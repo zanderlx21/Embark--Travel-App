d867744b1141ff0ee807e777e2e9ba0dbb1765de
Kat Gilbert <katgilbert99@gmail.com>
2022-02-04 09:51:10 -0500

    setting up files

---
+++ backend/functions/src/index.ts
@@ -0,0 +1,12 @@
+import * as functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+
+//import routes
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+export const api = functions.https.onRequest(app);
+
