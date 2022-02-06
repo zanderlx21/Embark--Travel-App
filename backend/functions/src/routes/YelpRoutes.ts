fc0edc06a941096be0808767de9f95be46acc6b3
Kat Gilbert <katgilbert99@gmail.com>
2022-02-06 13:50:57 -0500

    troubleshooting queries, api working on backend

--- backend/functions/src/routes/YelpRoutes.ts
+++ backend/functions/src/routes/YelpRoutes.ts
@@ -4,24 +4,20 @@ import fetch from 'node-fetch';
 
 export const yelpRoutes = express.Router(); 
 
-yelpRoutes.get("/search", async (req, res) => {
-//     try {
-//         const client = await getClient();
-//         const results = await client.db().collection("yelpSearch").find().toArray();
-//         res.json(results);
-//     } catch (err) {
-//         console.error("ERROR", err);
-//         res.status(500).json({ message: "Internal Server Error" });
-//         }
-// });
+yelpRoutes.get("/", async (req, res) => {
+
+    // const location = req.query.search
+    // console.log(location);
 
     try {
-        const api_url = "https://api.yelp.com/v3/businesses/search";
-        // {
-            //     params: { location: query },
-            //     headers: { 'Authorization': `Bearer ${yelpKey}` }
-            //   })
-        const fetch_response = await fetch(api_url);
+        const api_url = 'https://api.yelp.com/v3/businesses/search';
+
+        let location: string = req.query.location as string;
+        console.log(location);
+
+        const fetch_response = await fetch(api_url +location, {
+            headers: { 'Authorization': `Bearer QyWd6Rce5pLg83TEL20FxoVv8QF4MyQ6BHntGahHQ9LnOlsN1rJQ0V0Y5Z36Qw9FFKqH-qh_wGiS48rLJCElWhjLgt-4WrEOqVEWcNoUjL42pO8FyVonpSlsITr8YXYx` }
+            });
         const json = await fetch_response.json();
         console.log(json)
         res.json(json);
@@ -31,3 +27,16 @@ yelpRoutes.get("/search", async (req, res) => {
         res.status(500).json({message: "Internal Service Error"});
     }
 });
+
+
+
+
+//     try {
+//         const client = await getClient();
+//         const results = await client.db().collection("yelpSearch").find().toArray();
+//         res.json(results);
+//     } catch (err) {
+//         console.error("ERROR", err);
+//         res.status(500).json({ message: "Internal Server Error" });
+//         }
+// });
--- frontend/src/services/YelpAPIService.tsx
+++ frontend/src/services/YelpAPIService.tsx
@@ -2,17 +2,20 @@ import axios from "axios";
 import { Business } from "../models/YelpModel";
 
 const yelpKey = process.env.REACT_APP_YELP_API_KEY;
-// const yelpBaseUrl = "https://api.yelp.com/v3/businesses";
-//"https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses"
 
-// from other example
-const yelpBaseUrl = "/search";
-
-export const fetchBusinesses = async (query:string) => {
-const response = await fetch(yelpBaseUrl + "?");
+const yelpBaseUrl = "http://localhost:5001/travelapp-f2d81/us-central1/api/search";
+export const fetchBusinesses = async (query: string) => {
+const response = await fetch(yelpBaseUrl);
 const json = await response.json();
 console.log(json);
 }
+
+
+
+
+
+
+//"https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses"
 
 // export function fetchBusinesses(query:string): Promise<Business[]> {
   //for consoling logging purposes:
@@ -23,7 +26,6 @@ console.log(json);
   // })
   // .then( res => console.log(res.data) )
   
-
 //   return axios
 //   .get<Business[]>(("/search"), {
 //     params: { location: query },
