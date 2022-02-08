9d26630accb93539eaa5c6a6499ebeae399c9412
Kat Gilbert <katgilbert99@gmail.com>
2022-02-08 18:33:11 -0500

    added trashcan to delete line as placeholder

--- frontend/src/components/ItineraryItem.tsx
+++ frontend/src/components/ItineraryItem.tsx
@@ -9,12 +9,11 @@ interface Props {
 export function ItineraryItem({business, onDelete}:Props) {
 
     return (
-        <div onClick={onDelete}>
-<h1>ITINERARY LIST</h1>
+        <div className="ItineraryItem">
+            <h1>ITINERARY LIST</h1>
 
             <p>{business.name}</p>
-            <p>{business.rating}</p>
-            {/* <p><i className="material-icons" onClick={onDelete}>delete</i></p> */}
+            <p>{business.rating} <i className="material-icons" onClick={onDelete}>delete</i></p>
             
         </div>
     )
