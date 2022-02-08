14626b04e3be7b13d4589b6ee6c006b1be65a0e1
Alex Zaks <zaksa86@gmail.com>
2022-02-08 14:54:19 -0500

    pictures styled better

--- frontend/src/components/HotelResult.tsx
+++ frontend/src/components/HotelResult.tsx
@@ -9,9 +9,9 @@ export function HotelResult({business}:Prop) {
     return (
 
         <div className="Hotel-Result">
-            <img src="https://cdn.vox-cdn.com/thumbor/730qb_rXs4ngJ9PMfBpMo2uisHQ=/0x158:3000x1846/1600x900/cdn.vox-cdn.com/uploads/chorus_image/image/56295273/20150609-oleanders_restaurant_-5.0.0.jpg" />
+            <img src="https://cdn.vox-cdn.com/thumbor/730qb_rXs4ngJ9PMfBpMo2uisHQ=/0x158:3000x1846/1600x900/cdn.vox-cdn.com/uploads/chorus_image/image/56295273/20150609-oleanders_restaurant_-5.0.0.jpg" alt="image of Restaurant Lobby" />
             <p>{business.name}</p>
-            <div className="image"><img src={business.url} alt="" /></div>
+            {/* <div className="image"><img src={business.url} alt="" /></div> */}
             <p className="rating">{business.rating}</p>
 
         </div>
