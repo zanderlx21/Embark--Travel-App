3c08b872e1d90cfa6dfe8c56eb02cf4ea90ce31a
Alex Zaks <zaksa86@gmail.com>
2022-02-09 12:27:15 -0500

    Hotels slider formatted

--- frontend/src/components/ImageSlider.tsx
+++ frontend/src/components/ImageSlider.tsx
@@ -1,14 +1,15 @@
 import { HotelResult } from './HotelResult';
 import { Business } from '../models/YelpModel';
 import { addFavorite } from '../models/ItineraryPost';
+import { useState } from 'react';
 
 export interface MainProp {
     businesses: Business[];
 }
 
-const pos = { top: 0, left: 0, x: 0, y: 0 };
-
 function HotelResultsList ({businesses}:MainProp) {
+    const [current, setCurrent] = useState(0)
+    
 
     function addToFavorites(business: Business){
         addFavorite(business);
@@ -18,6 +19,7 @@ function HotelResultsList ({businesses}:MainProp) {
 
         <div className="Hotel-List">
             <h1>Hotels</h1>
+            
             
         <div className="Hotel-Map-Div">
         {businesses.map((business, i) => <HotelResult key={i} business={business} onFavorite={addToFavorites} />)}
